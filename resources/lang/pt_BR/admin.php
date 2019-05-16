b16b730962b9649e505873e680eb9cb6b2ac545b
Marcos Wolff <wmarcos@gmail.com>
2019-05-16 10:47:46 -0300

    Archivos traducción al portugués

---
+++ resources/lang/pt_BR/admin.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    //formularios utilitarios
+    'atras' => 'Back',
+    'editar' => 'Edit',
+    'eliminar' => 'Delete',
+    'confirmar' => 'Confirm',
+    'presente' => 'Present',
+    'crear' => 'Create',
+    'nueva' => 'New',
+    'nuevo' => 'New',
+    'listado_de' => 'Listado de ',
+
+    //actividades
+    'actividad' => 'Activity',
+    'actividades' => 'Activities',
+    'inscribir' => 'Suscribe',
+    'invitar' => 'Invite',
+    'seleccionar_usuario' => 'Select user',
+    'no_hay_evaluaciones' => 'No evaluations',
+    'no_hay_miembros' => 'No collaborators',
+    'no_hay_inscriptos' => 'No one suscribed',
+    'actividades_mias' => 'Created by me',
+    'actividades_invitado' => 'Shared with me',
+    'actividades_todas' => 'All',
+
+    //Menú actividad
+    'general' => 'General',
+    'inscripciones' => 'Suscriptions',
+    'evaluaciones' => 'Feedback',
+    'invitaciones' => 'Collaborators',
+    'auditoria' => 'Logs',
+    'puntos' => 'Meeting point',
+    'grupos' => 'Groups',
+
+    //formulario actividad
+    'nombre' => 'Name',
+    'descripcion' => 'Description',
+    'lugar' => 'Place',
+    'estado' => 'Status',
+    'visibilidad' => 'Visibility',
+    'tipo' => 'Type',
+    'inicio' => 'Starts',
+    'fin' => 'Ends',
+    'limite_inscripciones' => 'Suscribers limit',
+    'mensaje_inscripciones' => 'Message to suscribers',
+
+    //auditorías
+    'creo_actividad' => 'created activity',
+    'edito_actividad' => 'edited activitys',
+    'creo_inscripcion' => 'suscribed',
+    'edito_inscripcion' => 'edited suscription of ',
+    'elimino_inscripcion' => 'unsuscribed',
+    'se_inscribio' => 'was suscribed',
+
+    //Usuarios
+    'usuarios' => 'Users',
+    'administrar_usuarios' => 'Manage users',
+
+    //Sitio
+    'sitio' => 'Site',
+
+];
---
+++ resources/lang/pt_BR/auth.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Credenciais informadas não correspondem com nossos registros.',
+    'throttle' => 'Você realizou muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+    'register' => 'Register',
+    'nombre' => 'Name',
+    'apellido' => 'Surname',
+    'email' => 'E-mail Address',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'login' => 'Login',
+    'remember_me' => 'Remember Me',
+    'forgot_password' => 'Forgot Your Password?',
+    'dashboard' => 'Dashboard',
+    'logged_in' => 'You are logged in!',
+    'logout' => 'Logout'
+];
